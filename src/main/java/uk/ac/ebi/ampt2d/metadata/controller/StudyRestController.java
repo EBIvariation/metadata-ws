79d4892005db6406f724a17b258c52df9cf5dbdd
selva <selva@ebi.ac.uk>
2018-05-21 13:43:49 +0100

    added api tags

--- src/main/java/uk/ac/ebi/ampt2d/metadata/controller/StudyRestController.java
+++ src/main/java/uk/ac/ebi/ampt2d/metadata/controller/StudyRestController.java
@@ -1,6 +1,8 @@
 package uk.ac.ebi.ampt2d.metadata.controller;
 
 import com.querydsl.core.types.Predicate;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,11 +13,13 @@ import uk.ac.ebi.ampt2d.metadata.persistence.entities.Study;
 import uk.ac.ebi.ampt2d.metadata.persistence.repositories.StudyRepository;
 
 @RestController
+@Api(tags = "Study Entity")
 public class StudyRestController {
 
     @Autowired
     private StudyRepository studyRepository;
 
+    @ApiOperation(value = "studySearch")
     @RequestMapping(method = RequestMethod.GET, path = "/studies/search/")
     public Iterable<Study> getStudies(@RequestParam("searchString") String searchString) {
         QStudy study = QStudy.study;
