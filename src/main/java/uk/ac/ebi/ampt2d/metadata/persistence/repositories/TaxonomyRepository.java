9479f419f41b90f7444ef11a2bbc887a652fa438
Zhicheng-Liu <zl@ebi.ac.uk>
2018-06-06 10:15:51 +0100

    Remove TaxonomyRepository from extending QueryDslPredicateExecutor<>
    In previous commit, the dependency on TaxonomyRepository in StudyService
    to fetch taxonomies by id or name has been removed. So there is no more
    need to extend that interface.

--- src/main/java/uk/ac/ebi/ampt2d/metadata/persistence/repositories/TaxonomyRepository.java
+++ src/main/java/uk/ac/ebi/ampt2d/metadata/persistence/repositories/TaxonomyRepository.java
@@ -17,11 +17,10 @@
  */
 package uk.ac.ebi.ampt2d.metadata.persistence.repositories;
 
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import uk.ac.ebi.ampt2d.metadata.persistence.entities.Taxonomy;
 
 @RepositoryRestResource
-public interface TaxonomyRepository extends CrudRepository<Taxonomy, Long>, QueryDslPredicateExecutor<Taxonomy> {
+public interface TaxonomyRepository extends CrudRepository<Taxonomy, Long> {
 }
