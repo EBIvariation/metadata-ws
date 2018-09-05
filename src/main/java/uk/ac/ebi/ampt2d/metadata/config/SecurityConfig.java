4f5e27cf387f9d2a17115419b528e6e215c4313a
selva <selva@ebi.ac.uk>
2018-09-05 14:20:11 +0100

    exclude swagger from auth

--- src/main/java/uk/ac/ebi/ampt2d/metadata/config/SecurityConfig.java
+++ src/main/java/uk/ac/ebi/ampt2d/metadata/config/SecurityConfig.java
@@ -10,13 +10,24 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class SecurityConfig extends ResourceServerConfigurerAdapter {
 
+    private static final String[] AUTH_WHITELIST = {
+            // -- swagger ui
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            "/"
+    };
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
-
         http
                 .authorizeRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .antMatchers(HttpMethod.POST).hasAuthority("WRITE")
-                .antMatchers("/").permitAll()
                 .antMatchers("/**").authenticated();
 
     }
