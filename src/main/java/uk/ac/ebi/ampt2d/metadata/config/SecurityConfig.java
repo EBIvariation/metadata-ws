b56c11ad6b9960fd4c74dd4db517d417b6301206
selva <selva@ebi.ac.uk>
2018-09-13 16:21:27 +0100

    changed name for securityConfig

--- src/main/java/uk/ac/ebi/ampt2d/metadata/config/SecurityConfig.java
+++ src/main/java/uk/ac/ebi/ampt2d/metadata/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
-public class SecurityConfiguration {
+public class SecurityConfig {
 
     @ConditionalOnProperty(value = "security.enabled", havingValue = "true")
     @Configuration
