015268cde1037bd4b7be6c45c081518ab511d44d
Kirill Tsukanov <ktsukanov@ebi.ac.uk>
2019-05-17 10:10:43 +0100

    Review: modify SQL statement for Publication

--- metadata-ws/src/main/java/uk/ac/ebi/ampt2d/metadata/persistence/entities/Publication.java
+++ metadata-ws/src/main/java/uk/ac/ebi/ampt2d/metadata/persistence/entities/Publication.java
@@ -60,10 +60,9 @@ public class Publication extends Auditable<Long>{
     /**
      * Release date control: get the *earliest* release date from all studies which link to this publication.
      */
-    @Formula("(SELECT min(study.release_date) FROM publication " +
-             "INNER JOIN study_publications on publication.id = study_publications.study_id " +
-             "INNER JOIN study on study_publications.study_id = study.id" +
-             "WHERE publication.id=id)")
+    @Formula("(SELECT min(study.release_date) FROM study_publications " +
+             "INNER JOIN study on study_publications.study_id = study.id " +
+             "WHERE study_publications.publications_id=id)")
     @JsonIgnore
     private LocalDate releaseDate;
 
