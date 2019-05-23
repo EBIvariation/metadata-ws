01868e0f07c6e9ab05a0a7d947dea156a61ab384
Kirill Tsukanov <ktsukanov@ebi.ac.uk>
2019-05-23 10:47:10 +0100

    Implemented Sample converter

---
+++ metadata-load/src/main/java/uk/ac/ebi/ampt2d/metadata/importer/converter/SampleConverter.java
@@ -0,0 +1,35 @@
+/*
+ *
+ * Copyright 2019 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package uk.ac.ebi.ampt2d.metadata.importer.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import uk.ac.ebi.ampt2d.metadata.persistence.entities.AccessionVersionId;
+import uk.ac.ebi.ampt2d.metadata.persistence.entities.Sample;
+import uk.ac.ebi.ena.sra.xml.SampleType;
+
+public class SampleConverter implements Converter<SampleType, Sample> {
+
+    @Override
+    public Sample convert(SampleType sampleType) {
+        return new Sample(
+                new AccessionVersionId(sampleType.getAccession(), 1),
+                sampleType.getAlias()
+        );
+    }
+
+}
