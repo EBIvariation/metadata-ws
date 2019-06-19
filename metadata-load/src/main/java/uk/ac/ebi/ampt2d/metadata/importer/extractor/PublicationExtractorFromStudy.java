ad4e70b6afe19f73b57a06a133fff1f94b78225d
selva <selva@ebi.ac.uk>
2019-06-19 10:07:34 +0100

    pubmed check equalsignorecase

--- metadata-load/src/main/java/uk/ac/ebi/ampt2d/metadata/importer/extractor/PublicationExtractorFromStudy.java
+++ metadata-load/src/main/java/uk/ac/ebi/ampt2d/metadata/importer/extractor/PublicationExtractorFromStudy.java
@@ -45,7 +45,7 @@ public class PublicationExtractorFromStudy {
         LinkType[] studyLinksArray = studylinks.getSTUDYLINKArray();
         for (int i = 0; i < studyLinksArray.length; i++) {
             XRefType xRefType = studyLinksArray[i].getXREFLINK();
-            if (xRefType != null && xRefType.getDB().equals(PUB_MED)) {
+            if (xRefType != null && xRefType.getDB().equalsIgnoreCase(PUB_MED)) {
                 publications.add(findOrCreatePublication(xRefType.getID()));
             }
         }
